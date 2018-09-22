d39e0ecd0eb83f7812ff8e6654bdd9c9300d2ed5
Heather Miller <heather.miller114@gmail.com>
2018-09-22 19:34:09 +0000

    automatically backed up by learn

---
+++ lib/post.rb
@@ -0,0 +1,19 @@
+class Post
+  
+  attr_accessor :title, :author
+  @@all = []
+  
+  def initialize(title)
+    @title = title
+    @@all << title
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def author_name
+    self.author.name
+  end
+  
+end
